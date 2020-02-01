3938080a173257a1a06dfe57a2c5d4be7b173de8
Naoto Takezawa <naoto9march@gmail.com>
2020-02-01 10:51:54 +0900

    ロジックをmainの外へ&mainではエラーを処理するのみ

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
+use std::boxed::Box;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    //println!("{:?}", args);
 
-    if args.len() < 3 {
-      panic!("usage: minigrep query filename");
+    // 戻り値がある場合はunwrap_or_elseを使う
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("引数の解析時に問題が発生しました。{}", err);
+        process::exit(1);
+    });
+
+    // 戻り値がないので if let Err(e)を使う
+    if let Err(e) = run(config){
+      println!("アプリケーションでエラーが発生しました。{}", e);
+      process::exit(1);
     }
-
-    let config = Config::new(&args);
-
-    let contents = fs::read_to_string(config.filename)
-      .expect("ファイルを開くのに失敗しました。");
-    println!("テキストは : \n{}", contents);
 }
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> { // dyn = dynamic : implemsnts Errorの意味
+  let contents = fs::read_to_string(config.filename)?;
+
+  println!("テキストは : \n{}", contents);
+
+  Ok(()) // void?
+}
+
 
 struct Config {
   query : String,
@@ -22,7 +36,10 @@ struct Config {
 }
 
 impl Config {
-  fn new(args: &[String]) -> Config {
-    Config{query : args[1].clone(), filename : args[2].clone()}
+  fn new(args: &[String]) -> Result<Config, &'static str> {
+    if args.len() < 3 {
+      return Err("usage: minigrep query filename");
+    }
+    Ok(Config{query : args[1].clone(), filename : args[2].clone()}) // 所有権の移転を回避するため、やむなくcloneしている
   }
 }
